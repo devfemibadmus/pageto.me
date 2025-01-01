b510c3e14a0a195e44598852002bd1ddbff084b9
devfemibadmus <devfemibadmus@gmail.com>
2025-01-01 13:39:02 +0100

    🥳 pagin 🍎 ation 🔼

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, re_path
 
 urlpatterns = [
     path('', views.home, name="home"),
-    re_path(r'^.*$', views.home, name="lost"),
+    re_path(r'^account.*$', views.account, name="account"),
+    re_path(r'^analytics.*$', views.analytics, name="analytics"),
+    re_path(r'^templates.*$', views.templates, name="templates"),
+    re_path(r'^.*$', views.lost, name="lost"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'home.html')
+
+def lost(request):
+    return redirect('home')
+
+def account(request):
+    return render(request, 'home.html')
+
+def analytics(request):
+    return render(request, 'home.html')
+
+def templates(request):
+    return render(request, 'home.html')
+
