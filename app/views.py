f5a6e36fdc5376001f08a19f1cf8f6892f1ebced
devfemibadmus <devfemibadmus@gmail.com>
2025-01-02 16:39:30 +0100

    🌍 test  🚑️ views 🔒

--- app/views.py
+++ app/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'home.html')
 
-def lost(request):
-    return redirect('home')
-
 def account(request):
-    return render(request, 'home.html')
+    return render(request, 'account.html')
 
 def analytics(request):
-    return render(request, 'home.html')
+    return render(request, 'analytics.html')
 
 def templates(request):
-    return render(request, 'home.html')
+    return render(request, 'templates.html')
+
+def lost(request):
+    return redirect('home')
 
